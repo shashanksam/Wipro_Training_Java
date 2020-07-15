1c6632349a1e016d25143f2dc1a95ef54d63ccc1
ShashankSamS <35063506+shashanksam@users.noreply.github.com>
2020-07-15 22:48:02 +0530

    Assign13_1 completed file.

--- Assessments/Assigmnent13/Assign13_1.java
+++ Assessments/Assigmnent13/Assign13_1.java
@@ -1,2 +1,33 @@
-import java.out.*;
+// Java 8
 //TAKE THREE NUMBERS FROM THE USER AND PRINT THE GREATEST NUMBER
+import java.util.Scanner;
+
+public class Assign13_1 {
+	public static void main (String[] args) {
+		System.out.print("\nEnter first number: ");
+    Scanner inp = new Scanner(System.in);
+		int n1,n2,n3, max;
+		n1=inp.nextInt();
+		max=n1;
+		System.out.print("\nEnter second number: ");
+    n2=inp.nextInt();
+		if(max<n2) max=n2;
+		System.out.print("\nEnter third number: ");
+    n3=inp.nextInt();
+		if(max<n3) max=n3;
+		System.out.println("\nLargest number is "+max+".");
+	}
+}            
+                       
+/*
+inputs
+126
+5489
+654
+Output:
+
+Enter first number: 
+Enter second number: 
+Enter third number: 
+Largest number is 5489.
+*/
